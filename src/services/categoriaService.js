cb412619541e6beba866dc707bac0ab937f20462
duarterailla <railladuarte@hotmail.com>
2025-08-19 09:48:21 -0300

    Atualizações api

--- src/services/categoriaService.js
+++ src/services/categoriaService.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import api from './api';
 
 export const listarCategorias = async () => {
@@ -9,21 +9,4 @@ export const listarCategorias = async () => {
 export const cadastrarCategoria = async (categoria) => {
   const { data } = await api.post('/api/categorias', categoria);
   return data;
-};
-
-export const atualizarCategoria = async (id, categoria) => {
-  const { data } = await api.put(`/api/categorias/${id}`, categoria);
-  return data;
-=======
-import api from './api';
-
-export const listarCategorias = async () => {
-  const { data } = await api.get('/api/categorias');
-  return data;
-};
-
-export const cadastrarCategoria = async (categoria) => {
-  const { data } = await api.post('/api/categorias', categoria);
-  return data;
->>>>>>> 75b9b479a453719b25ebf1ec8155f7004fe16684
 };
