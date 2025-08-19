b84f66e84361379c95e21b461df7928eae9db7a0
duarterailla <railladuarte@hotmail.com>
2025-08-19 09:35:16 -0300

    Salvando alterações

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
@@ -8,14 +6,3 @@ const PrivateRoute = ({ children }) => {
 };
 
 export default PrivateRoute;
-=======
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-const PrivateRoute = ({ children }) => {
-	const isAuthenticated = !!localStorage.getItem('userId');
-	return isAuthenticated ? children : <Navigate to="/login" replace />;
-};
-
-export default PrivateRoute;
->>>>>>> 75b9b479a453719b25ebf1ec8155f7004fe16684
